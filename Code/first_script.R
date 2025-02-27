c22a49d9331e02e1c5edf1dc1bd0a58f4aba8c4f
lorenzofrancavilla <lollofranca5@ICLOUD.COM>
2025-02-27 16:01:43 +0100

    Update first_script.R

--- Code/first_script.R
+++ Code/first_script.R
@@ -4,3 +4,27 @@
 
 anna  <-   2+3 # assign an operation to an object
 chiara <- 4+6
+
+filippo  <-   c(0.2, 0.4, 0.6, 0.8, 0.9)  # array
+luca  <- c(100, 80, 60, 50, 10)
+
+plot (luca, filippo) #grafico x-luca, y-filippo 
+plot (luca, filippo, pch=19)
+plot (luca, filippo, pch=19, col= "blue")
+plot (luca, filippo, pch=19, col= "blue", cex=2, xlab= "rubbish",ylab="biomass")
+
+
+
+
+# Installing packages
+# CRAN
+install.packages("terra")
+library(terra)
+
+install.packages ("devtools")
+library (devtools)
+
+install_github("ducciorocchini/imageRy")
+
+
+# Rtools 4.4
