12e249b686ba7197f95c830700c4b63bf42ca538
lorenzo francavilla <lollofranca5@ICLOUD.COM>
2025-03-20 15:53:56 +0100

    Update 04_multitemp_analysis.R

--- Code/04_multitemp_analysis.R
+++ Code/04_multitemp_analysis.R
@@ -1,7 +1,7 @@
 # R code for performing multitemporal analysis
 
 
-
+install.packages("ggridges") #needed to create ridgeline plots
 library(imageRy)
 library(terra)
 library(viridis)
@@ -39,3 +39,50 @@ plot(grdif)
 
 # le temperature piu basse aumentano
 
+#install ggridges
+# ridgeline plot - (https://en.wikipedia.org/wiki/Ridgeline_plot)
+# distribuzione delle frequenze nel tempo 
+# sfondo grigio è ggplot
+
+
+# ridgeline plots 
+
+im.ridgeline(gr, scale=1)
+im.ridgeline(gr, scale=2)
+im.ridgeline(gr, scale=2, palette="plasma")
+im.ridgeline(gr, scale=3, palette="plasma")
+
+# importing ndvi sentinel images
+
+sent = im.import("Sentinel2")
+
+im.ridgeline(sent, scale=2)
+
+# changing names
+# sources :     Sentinel2_NDVI_2020-02-21.tif  
+              # Sentinel2_NDVI_2020-05-21.tif  
+              # Sentinel2_NDVI_2020-08-01.tif  
+              # Sentinel2_NDVI_2020-11-27.tif 
+
+names(sent) = c("02_Feb", "05_May", "08_Aug", "11_Nov")
+
+im.ridgeline(sent, scale=2, palette="mako")
+
+pairs(sent)
+
+plot(sent[[1]], sent[[2]])
+# y = x May=y, Feb=x 
+# y = a + bx (intercetta = dove linea tocca asse y)
+# a=0, b=1
+#  y = a + bx = 0 + 1x = x 
+
+abline(0, 1, col="red") # come vengono distribuiti i dati 
+
+plot(sent[[1]], sent[[2]], xlim=c(-0.3,0.9), ylim=c(-0.3,0.9))
+abline(0, 1, col="red")
+
+im.multiframe(1,3)
+plot(sent[[1]])
+plot(sent[[2]])
+plot(sent[[1]], sent[[2]], xlim=c(-0.3,0.9), ylim=c(-0.3,0.9))
+abline(0, 1, col="red") 
